41a68ad3f3e2d5ce24fbfa04224fcd271d49be27
Andrzej Michalski <113372492+Michalski84@users.noreply.github.com>
2023-02-26 21:19:48 +0100

    z1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const gallery = document.querySelector(".gallery");
+const galleryItem = galleryItems
+  .map(
+    (item) =>
+          `<div class="gallery__item">
+      <a class="gallery__link" href="${item.original}"><img src="${item.preview}" data-source="${item.original}" class="gallery__image" alt="${item.description}"</img></a></div>`
+  )
+  .join("");
+gallery.innerHTML = (galleryItem);
+
+document.querySelector('.gallery').onclick = (event) => {
+ event.preventDefault();
+	basicLightbox.create(`
+		<img width="1400" height="900" src="${event.target.dataset.source}">
+	`).show()
+
+}
