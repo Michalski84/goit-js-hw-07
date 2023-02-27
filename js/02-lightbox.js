db1fb9c536c0f5e687e789c946eeb6ec486b83c7
Andrzej Michalski <113372492+Michalski84@users.noreply.github.com>
2023-02-27 21:16:03 +0100

    z2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const galleryItem = galleryItems
+  .map(
+    (item) =>   
+      `<a class="gallery__item" href="${item.original}"><img src="${item.preview}" class="gallery__image" alt="${item.description}"</img></a>`
+  )
+  .join("");
+gallery.innerHTML = (galleryItem);
+
+document.querySelector('.gallery').onclick = (event) => {
+ event.preventDefault();
+	basicLightbox.create(`
+		<img width="1400" height="900" src="${event.target.dataset.source}">
+	`).show()
+
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+
+});
